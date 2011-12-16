c27b003a030d9cc5219b0976251a6b85dffff673
Dave Yeu <daveyeu@gmail.com>
2011-12-15 23:57:21 -0500

    Fix and simplify recent SMS response

--- lib/google/voice/sms.rb
+++ lib/google/voice/sms.rb
@@ -4,56 +4,32 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'base')
 GOOGLE_VOICE_SMS_TYPE = 11
 
 module Google
-  module Voice      
-    class Sms < Base    
+  module Voice
+    class Sms < Base
       def sms(number, text)
-        @curb.http_post([ 
+        @curb.http_post([
           Curl::PostField.content('phoneNumber', number),
           Curl::PostField.content('text', text),
-          Curl::PostField.content('_rnr_se', @_rnr_se) 
+          Curl::PostField.content('_rnr_se', @_rnr_se)
         ])
-        @curb.url = "https://www.google.com/voice/sms/send"        
+        @curb.url = "https://www.google.com/voice/sms/send"
         @curb.perform
         @curb.response_code
       end
-      
+
       def recent
-        @curb.url = "https://www.google.com/voice/inbox/recent/"        
+        @curb.url = "https://www.google.com/voice/inbox/recent/"
         @curb.http_get
-        sms = []
         doc = Nokogiri::XML::Document.parse(@curb.body_str)
-        data = doc.xpath('/response/json').first.text
         html = Nokogiri::HTML::DocumentFragment.parse(doc.to_html)
-        json = JSON.parse(data)        
-        # Format for messages is [id, {attributes}]
-        json['messages'].each do |message|
-          if message[1]['type'].to_i != GOOGLE_VOICE_SMS_TYPE
-            next
-          else
-            messages = []            
-            html.css('div.gc-message-sms-row').each do |row|
-              if row.css('span.gc-message-sms-from').inner_html.strip! =~ /Me:/
-                next
-              elsif row.css('span.gc-message-sms-time').inner_html =~ Regexp.new(txt_obj.display_start_time)
-                messages << {
-                  :to => 'Me',
-                  :from => row.css('span.gc-message-sms-from').inner_html.strip!.gsub!(':', ''),
-                  :text => row.css('span.gc-message-sms-text').inner_html,
-                  :time => row.css('span.gc-message-sms-time').inner_html
-                }
-              end
-            end
-            # Google is using milliseconds since epoch for time
-            sms << {
-              :id => message[0],
-              :phone_number => message[1]['phoneNumber'],
-              :start_time => Time.at(message[1]['startTime'].to_i / 1000),
-              :messages => messages} 
+        html.css("div.gc-message-sms-row").map do |row|
+          {}.tap do |message|
+            message[:from] = row.css(".gc-message-sms-from").inner_html.strip
+            message[:text] = row.css(".gc-message-sms-text").inner_html.strip
+            message[:time] = row.css(".gc-message-sms-time").inner_html.strip
           end
-        end      
-        sms
-      end      
-      
+        end
+      end
     end
   end
 end
