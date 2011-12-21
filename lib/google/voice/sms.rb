e3bf0fb05b5f01efd369b3030bd7f22c1a0992fb
Dave Yeu <daveyeu@gmail.com>
2011-12-20 22:40:55 -0500

    Fetching recent SMS now returns a Hash of telephone numbers to messages
    This helps separate the conversations.

--- lib/google/voice/sms.rb
+++ lib/google/voice/sms.rb
@@ -17,18 +17,27 @@ module Google
         @curb.response_code
       end
 
+      # Returns a Hash of telephone number => messages.
       def recent
         @curb.url = "https://www.google.com/voice/inbox/recent/"
         @curb.http_get
+        results = {}
+
         doc = Nokogiri::XML::Document.parse(@curb.body_str)
         html = Nokogiri::HTML::DocumentFragment.parse(doc.to_html)
-        html.css("div.gc-message-sms-row").map do |row|
-          {}.tap do |message|
-            message[:from] = row.css(".gc-message-sms-from").inner_html.strip
-            message[:text] = row.css(".gc-message-sms-text").inner_html.strip
-            message[:time] = row.css(".gc-message-sms-time").inner_html.strip
+        html.css("table.gc-message-tbl").each do |table|
+          telephone_number = table.css("td.gc-message-tbl-metadata a.gc-message-name-link").inner_html.strip
+          messages = table.css("div.gc-message-sms-row").map do |row|
+            {}.tap do |message|
+              message[:from] = row.css(".gc-message-sms-from").inner_html.strip.gsub(/:$/, "")
+              message[:text] = row.css(".gc-message-sms-text").inner_html.strip
+              message[:time] = row.css(".gc-message-sms-time").inner_html.strip
+            end
           end
+          results[telephone_number] = messages
         end
+
+        results
       end
     end
   end
